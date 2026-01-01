f880ff1035b39c6184df9b63c480f43037a8c8f9
qminh011002 <qminh.workmode@gmail.com>
2026-01-01 14:45:05 +0700

    UPDATE

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -2,6 +2,21 @@
  * product controller
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController(
+  "api::product.product",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { data, meta } = await super.find(ctx);
+
+      const cleanData = data.map((item) => ({
+        id: item.id,
+        title: item.title,
+        price: item.price,
+      }));
+
+      return { data: cleanData, meta };
+    },
+  })
+);
