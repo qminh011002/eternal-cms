dbac6f3894e80cc500bcd18466a2149043e07a3b
qminh011002 <qminh.workmode@gmail.com>
2026-01-03 00:10:33 +0700

    Add content security policy directives for Cloudinary integration

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,31 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          'connect-src': [
+            "'self'",
+            'https://api.cloudinary.com',
+            'https://res.cloudinary.com',
+          ],
+        },
+      },
+    },
+  },
 ];
